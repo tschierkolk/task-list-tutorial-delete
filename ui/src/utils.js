15e7e084d0953ae5ed1fb9471fffd07a1be48c4d
tschierkolk <155027412+tschierkolk@users.noreply.github.com>
2024-01-04 02:11:37 -0700

    east

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,6 +1,4 @@
 export const API_URL = 
 <<<<<<< HEAD
-"https://yatvabgatysjiwn4rb5scpqfem0rxxar.lambda-url.us-east-1.on.aws/Task";
-=======
-"https://https://yatvabgatysjiwn4rb5scpqfem0rxxar.lambda-url.us-east-1.on.aws/";
->>>>>>> ca2b0e4c119eb09e5dfe1b148204db84588224ae
+"https://bods2vbyeevckfwymeum63vkky0vwhmk.lambda-url.us-east-1.on.aws/task";
+
